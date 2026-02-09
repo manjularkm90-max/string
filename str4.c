c2152e1b7fa3e015f3d6d84849d0b991de796ff0
manjularkm90-max <manjularkm90@gmail.com>
2026-02-09 22:20:03 +0530

    Add files via upload

---
+++ str4.c
@@ -0,0 +1,12 @@
+//reverse
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i, len;
+    scanf("%s", str);
+    len = strlen(str);
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+}
