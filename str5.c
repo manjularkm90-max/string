1036b479a6fd6dd14f564644fd693709729999bf
manjularkm90-max <manjularkm90@gmail.com>
2026-02-10 09:54:10 +0530

    Add files via upload

---
+++ str5.c
@@ -0,0 +1,13 @@
+//vowel counter
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char ch[50];
+    scanf("%s",ch);
+    int count=0;
+    for(int i=0;ch[i]!='\0';i++){
+        if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'||ch[i]=='A'||ch[i]=='E'||ch[i]=='I'||ch[i]=='O'||ch[i]=='U'){
+            count++;
+        }
+    }printf("%d",count);
+}
